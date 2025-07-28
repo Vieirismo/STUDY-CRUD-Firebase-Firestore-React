7f414cf63a739b6183a49283a0687ae04049093a
Vieira <tsuneosebajunvieira@gmail.com>
2025-07-27 23:53:15 -0300

    Day 3.1
    Some final back-end code. I don't want to use too many js extensions, as the interface becomes useless

---
+++ estudos_react/src/interfaces/plansInterface.js
@@ -0,0 +1,10 @@
+// import { doc } from 'firebase/firestore';
+
+const planType = {
+    text: String,
+    type: String,
+    value: Number,
+    // use referencia do id da coleção, no caso:
+    // const planId = doc(db, 'plans_refference', 'planIdRef'); 
+    // plan: planId
+}
